f5d7cd4fd4bfdb9b1f0f24cedd8c8022c7212c03
Wei Hao Lim <s10185304@connect.np.edu.sg>
2020-11-13 11:19:48 +0800

    Fixed the Raw Request issue. Made the Admin Menu thing. Linked with Admin account. Made a table for RawRequest

--- RQST/RQST/Controllers/AdminController.cs
+++ RQST/RQST/Controllers/AdminController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RQST.DAL;
+using RQST.Models;
 
 namespace RQST.Controllers
 {
     public class AdminController : Controller
     {
+        private DataDAL DataDALContext = new DataDAL();
         public IActionResult Admin()
         {
             return View();
+        }
+
+        public IActionResult RawRequests()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> RawRequestsAsync()
+        {
+            string auth = HttpContext.Session.GetString("auth");
+            List<Request> reqlist = await DataDALContext.getdata(auth);     //Gets authentication token (in JSON) and passes it to the DAL function getdata
+            return View(reqlist);                                           //Returns the list to the view
         }
     }
 }
