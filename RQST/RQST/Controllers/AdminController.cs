8dbaab54f203db80e6f407aa8fc3e11bb57761c5
Harvinder Arjun Singh S S <s10196189@connect.np.edu.sg>
2021-01-06 22:44:44 +0800

    Edited map screen, so that requests show up as circles representing the district they are in. Removed some useless files.

--- RQST/RQST/Controllers/AdminController.cs
+++ RQST/RQST/Controllers/AdminController.cs
@@ -152,17 +152,18 @@ namespace RQST.Controllers
             List<Area> arealist = new List<Area>();
             foreach (UserRequests usrqst in something)
             {
-                string prefix = usrqst.PostalCode.Substring(0, 2);
+                string prefix = usrqst.PostalCode.Substring(0, 2);  //Get 1st 2 digits of postal code
                 int pre = Convert.ToInt32(prefix);
-                if (pre < 7)
+                if (pre < 7)        //Checks postal code prefix
                 {
-                    Area oldarea = arealist.Find(x => x.AreaName == "Telok Blangah, Harbourfront");
-                    if (oldarea == null)
+                    //Tries to find area based on name
+                    Area oldarea = arealist.Find(x => x.AreaName == "Raffles Place, Cecil, Marina, People's Park");
+                    if (oldarea == null)    //If area does not exist create area
                     {
-                        oldarea = new Area("Telok Blangah, Harbourfront");
+                        oldarea = new Area("Raffles Place, Cecil, Marina, People's Park", 1, 1.285121,103.865261,1391.31);
                         arealist.Add(oldarea);
                     }
-                    oldarea.AddReq(usrqst);
+                    oldarea.AddReq(usrqst); //If it do add request to area
                     foreach (var item in usrqst.itemlist)
                     {
                         oldarea.AddItem(item);
@@ -173,7 +174,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Anson, Tanjong Pagar");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Anson, Tanjong Pagar");
+                        oldarea = new Area("Anson, Tanjong Pagar", 2,1.276060,103.840770,1390.16);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -187,7 +188,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Telok Blangah, Harbourfront");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Telok Blangah, Harbourfront");
+                        oldarea = new Area("Telok Blangah, Harbourfront",4,1.260455,103.816247,1575.08);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -201,7 +202,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Pasir Panjang, Hong Leong Garden, Clementi New Town");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Pasir Panjang, Hong Leong Garden, Clementi New Town");
+                        oldarea = new Area("Pasir Panjang, Hong Leong Garden, Clementi New Town",5,1.289376,103.765630,3594.95);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -215,7 +216,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Bukit Merah, Queenstown, Tiong Bahru");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Bukit Merah, Queenstown, Tiong Bahru");
+                        oldarea = new Area("Bukit Merah, Queenstown, Tiong Bahru",3,1.287417,103.813870,1258.08);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -229,7 +230,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "High Street, Beach Road(part)");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("High Street, Beach Road(part)");
+                        oldarea = new Area("High Street, Beach Road(part)",6,1.291077,103.848087,432.97);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -243,7 +244,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Middle Road, Golden Mile");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Middle Road, Golden Mile");
+                        oldarea = new Area("Middle Road, Golden Mile",7,1.299355,103.854196,440.02);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -257,7 +258,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Little India, Farrer Park, Jalan Besar, Lavender");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Little India, Farrer Park, Jalan Besar, Lavender");
+                        oldarea = new Area("Little India, Farrer Park, Jalan Besar, Lavender",8,1.306101,103.848052,521.22);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -271,7 +272,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Orchard, Cairnhill, River Valley");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Orchard, Cairnhill, River Valley");
+                        oldarea = new Area("Orchard, Cairnhill, River Valley",9, 1.303492,103.831832,1050.75);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -285,7 +286,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Ardmore, Bukit Timah, Holland Road, Tanglin");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Ardmore, Bukit Timah, Holland Road, Tanglin");
+                        oldarea = new Area("Ardmore, Bukit Timah, Holland Road, Tanglin",10,1.316878,103.808821,1484.54);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -299,7 +300,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Watten Estate, Novena, Thomson");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Watten Estate, Novena, Thomson");
+                        oldarea = new Area("Watten Estate, Novena, Thomson",11,1.327624,103.832866,1383.73);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -313,7 +314,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Balestier, Toa Payoh, Serangoon");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Balestier, Toa Payoh, Serangoon");
+                        oldarea = new Area("Balestier, Toa Payoh, Serangoon",12,1.320075,103.857490,1307.81);
                         arealist.Add(oldarea);
                         
                     }
@@ -328,7 +329,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Macpherson, Braddell");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Macpherson, Braddell");
+                        oldarea = new Area("Macpherson, Braddell",13, 1.328716, 103.877371,1132.26);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -342,7 +343,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Geylang, Eunos, Aljunied");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Geylang, Eunos, Aljunied");
+                        oldarea = new Area("Geylang, Eunos, Aljunied",14, 1.333101, 103.9002822, 1758.74);
                         arealist.Add(oldarea);
                     }
                     else
@@ -360,7 +361,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Katong, Joo Chiat, Amber Road");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Katong, Joo Chiat, Amber Road");
+                        oldarea = new Area("Katong, Joo Chiat, Amber Road",15, 1.297363, 103.897392,2200.54);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -374,7 +375,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Bedok, Upper East Coast, Eastwood, Kew Drive");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Bedok, Upper East Coast, Eastwood, Kew Drive");
+                        oldarea = new Area("Bedok, Upper East Coast, Eastwood, Kew Drive",16, 1.320215,103.932891,1660.25);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -388,7 +389,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Loyang, Changi");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Loyang, Changi");
+                        oldarea = new Area("Loyang, Changi",17,1.351193,103.991776,3384.61);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -402,7 +403,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Simei, Tampines, Pasir Ris");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Simei, Tampines, Pasir Ris");
+                        oldarea = new Area("Simei, Tampines, Pasir Ris",18, 1.358760,103.939414,2192.53);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -416,7 +417,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Serangoon Garden, Hougang, Punggol");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Serangoon Garden, Hougang, Punggol");
+                        oldarea = new Area("Serangoon Garden, Hougang, Punggol",19,1.373012,103.898936,3354.03);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -430,7 +431,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Bishan, Ang Mo Kio");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Bishan, Ang Mo Kio");
+                        oldarea = new Area("Bishan, Ang Mo Kio",20,1.359630,103.839218,1940.24);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -444,7 +445,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Upper Bukit Timah, Clementi Park, Ulu Pandan");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Upper Bukit Timah, Clementi Park, Ulu Pandan");
+                        oldarea = new Area("Upper Bukit Timah, Clementi Park, Ulu Pandan", 21,1.335503,103.789779,2009.37);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -458,7 +459,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Penjuru, Jurong, Pioneer, Tuas");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Penjuru, Jurong, Pioneer, Tuas");
+                        oldarea = new Area("Penjuru, Jurong, Pioneer, Tuas", 22,1.315477,103.661500,5509.66);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -472,7 +473,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Hillview, Dairy Farm, Bukit Panjang, Choa Chu Kang");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Hillview, Dairy Farm, Bukit Panjang, Choa Chu Kang");
+                        oldarea = new Area("Hillview, Dairy Farm, Bukit Panjang, Choa Chu Kang",23,1.361798,103.756060,3550.96);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -486,7 +487,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Lim Chu Kang, Tengah");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Lim Chu Kang, Tengah");
+                        oldarea = new Area("Lim Chu Kang, Tengah",24,1.407104,103.700098,3975.65);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -500,7 +501,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Kranji, Woodgrove, Woodlands");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Kranji, Woodgrove, Woodlands");
+                        oldarea = new Area("Kranji, Woodgrove, Woodlands",25,1.423270,103.773422,3250.99);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -514,7 +515,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Upper Thomson, Springleaf");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Upper Thomson, Springleaf");
+                        oldarea = new Area("Upper Thomson, Springleaf", 26,1.397369,103.819582,2367.58);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -528,7 +529,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Yishun, Sembawang, Senoko");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Yishun, Sembawang, Senoko");
+                        oldarea = new Area("Yishun, Sembawang, Senoko", 27,1423655,103.840142,2401.0);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
@@ -542,7 +543,7 @@ namespace RQST.Controllers
                     Area oldarea = arealist.Find(x => x.AreaName == "Seletar");
                     if (oldarea == null)
                     {
-                        oldarea = new Area("Seletar");
+                        oldarea = new Area("Seletar", 28,1.409982,103.877239,1940.36);
                         arealist.Add(oldarea);
                     }
                     oldarea.AddReq(usrqst);
--- RQST/RQST/Models/Area.cs
+++ RQST/RQST/Models/Area.cs
@@ -8,6 +8,10 @@ namespace RQST.Models
     public class Area
     {
         public string AreaName { get; set; }
+        public int AreaNum { get; set; }
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+        public double Radius { get; set; }
         public List<items> itemsList { get; set; } = new List<items>();
         public void AddItem(items item)
         {
@@ -18,9 +22,13 @@ namespace RQST.Models
         {
             reqlist.Add(req);
         }
-        public Area(string areaname)
+        public Area(string areaname, int areanum, double lat, double lng, double radius)
         {
             AreaName = areaname;
+            AreaNum = areanum;
+            Lat = lat;
+            Lng = lng;
+            Radius = radius;
         }
     }
 }
