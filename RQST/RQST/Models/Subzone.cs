40588893794963a8124b8b0df3ad9c769f77e971
Harvinder Arjun Singh S S <s10196189@connect.np.edu.sg>
2021-01-07 19:10:10 +0800

    Added subzones geojson file, in addition to subzone class

---
+++ RQST/RQST/Models/Subzone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RQST.Models
+{
+    public class Subzone
+    {
+        public string Name { get; set; }
+        public string SubzoneCode { get; set; }
+        public List<List<double>> coordinates { get; set; }
+    }
+}
