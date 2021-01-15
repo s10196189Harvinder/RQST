283db2d98f75d227546703245e0b2e35bca31fb3
Kai Hui Ang <s10198138@connect.np.edu.sg>
2021-01-15 22:24:23 +0800

    wip sort for list of elderlies

--- RQST/RQST/wwwroot/js/sortingElderly.js
+++ RQST/RQST/wwwroot/js/sortingElderly.js
@@ -48,3 +48,16 @@
         }
     }
 } 
+
+
+
+function myFunction() {
+    const input = document.getElementById("myInput");
+    const inputStr = input.value.toUpperCase();
+    document.querySelectorAll('#myTable tr:not(.header)').forEach((tr) => {
+        const anyMatch = [...tr.children]
+            .some(td => td.textContent.toUpperCase().includes(inputStr));
+        if (anyMatch) tr.style.removeProperty('display');
+        else tr.style.display = 'none';
+    });
+}
