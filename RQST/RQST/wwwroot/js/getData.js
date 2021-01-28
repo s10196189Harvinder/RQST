0e9f579846fb909c479282f64cf91c1f15480171
Arjun <s10196189@connect.np.edu.sg>
2021-01-28 21:13:37 +0800

    Fixed bug in map page where if user puts new data into firbase map would not update

--- RQST/RQST/wwwroot/js/getData.js
+++ RQST/RQST/wwwroot/js/getData.js
@@ -3,29 +3,33 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/maphub").build();
 connection.start();
 var data;
 connection.on("getData", function (obj) {
-    data = obj;
-    console.log(obj);
-    var found = objlist.findIndex(x => x.name == data.zoneID);
+    data = JSON.parse(String.fromCharCode.apply(null, obj));
+    var found = objlist.findIndex(x => x.name == data.ZoneID);
     if (found > -1) {
+        console.log("removed");
         objlist.splice(found, 1);   //remove obj from obj list for re-doing
     }
-    var words = "<div class='infotitle'>Total Requests: " + data.reqList.length + "</div></br>Item summary</br>"
-    for (var item in data.itemList) {
+    var words = "<div class='infotitle'>Total Requests: " + data.ReqList.length + "</div></br>Item summary</br>"
+    for (var item in data.ItemList) {
         words += (
             "<span class='itemname'>" +
-            data.itemList[item].name +
+            data.ItemList[item].name +
             "</span> : <span class='itemamnt'>" +
-            data.itemList[item].requested + "</span></br>"
+            data.ItemList[item].requested + "</span></br>"
         )
     }
     var newobj = {
-        name: obj.zoneID,
-        requests: obj.reqList.length,
+        name: data.ZoneID,
+        requests: data.ReqList.length,
         itemliststring: words
     }
+    console.log(newobj.name);
     map.data.forEach(function (feature) {
         var SD_NAME = feature.getProperty('Name');
-        if (newobj.name == SD_NAME) {
+        if ((data.ZoneID) == SD_NAME) {
+            console.log("same found !!");
+            console.log(newobj.requests);
+            console.log(newobj.itemliststring);
             feature.setProperty("requests", newobj.requests);
             feature.setProperty("itemliststring", newobj.itemliststring);
         }
